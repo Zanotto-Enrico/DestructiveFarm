619284daa0e677be9c84cee58c0ddc4bae14b4e9
Zanotto-Enrico <887566@stud.unive.it>
2023-06-28 23:41:20 +0200

    changed example

--- client/example.py
+++ client/example.py
@@ -13,26 +13,31 @@ import sys
 host = sys.argv[1]
 print("I need to attack a team with host: {}".format(host))
 
-service = 'NOME_SERVIZIO_API'
+service = 'SERVICE NAME'
 
 # Server supplying the flag's ids
 server = 'http://10.10.0.1:8081/flagIds?service='+service
 
-
 # requesting flags ids
-response = requests.get(server)
-data = response.json()
 
-# for every host the APIs returns 5 ids 
+response = requests.get(server)
+data = response.json();
+
+# for every host the APIs returns 5 ids
 # (runnin 5 exploit on the same host)
 for i in range(5):
-
-    # id to use for the service
     id = data[service][host][i]
     print(id)
-    
-    """
-    EXPLOIT
-    """
+    #       ...      ...
 
-    print("<FLAG>", flush=True)
+
+    proc = remote(host, 1234)
+
+    # ...
+
+    proc.write(b""  + b"\n")
+    proc.recvuntil(b"\n")
+
+    # ...
+
+    print(proc.recvuntil(b"="), flush=True)
